c3bb28e7c09898649f8e34b8486e2aac892dacc5
Alex Oliinyk <alex.p.oliinyk@gmail.com>
2025-02-15 15:59:21 +0100

    Codility project skeleton.
    Created skeleton of the project for solving Codility problems.

---
+++ src/main/java/ua/infinity/codility/Test.java
@@ -0,0 +1,18 @@
+package ua.infinity.codility;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Alex Oliinyk
+ * @see Object
+ * @since X.Y.Z
+ */
+public class Test {
+
+    private static final Logger logger = LoggerFactory.getLogger(Test.class);
+
+    public static void main(String[] args) {
+        logger.info("Hello, {}!", "World");
+    }
+}
---
+++ src/test/java/ua/infinity/codility/TestTest.java
@@ -0,0 +1,18 @@
+package ua.infinity.codility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Alex Oliinyk
+ * @see Object
+ * @since X.Y.Z
+ */
+class TestTest {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
